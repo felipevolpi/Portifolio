ed92e8bbc7b6fc56b110c712e6e90d434a4120cf
felipevolpi <felipe.volpi92@gmail.com>
2021-08-27 18:52:25 -0300

    botao

---
+++ mobile.navbar.js
@@ -0,0 +1,7 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+btnMobile.addEventListener('click', toggleMenu);
